f7eedd1d0183a113be8da3149674fb1f70ce8e3b
Ramy Carter <tbone425@msn.com>
2020-02-22 18:23:17 -0800

    add color question and logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
     $("button#green").click(function() {
-        $()
-    })
-})
+        $("body").removeClass();
+        $("body").addClass("green-background");
+    });
+
+    $("button#blue").click(function() {
+        $("body").removeClass();
+        $("body").addClass("blue-background");
+    });
+
+    $("button#red").click(function() {
+        $("body").removeClass();
+        $("body").addClass("red-background");
+    });
+
+    $("button#yellow").click(function() {
+        $("body").removeClass();
+        $("body").addClass("yellow-background");
+    });
+
+    $("button#orange").click(function() {
+        $("body").removeClass();
+        $("body").addClass("orange-background");
+    });
+});
