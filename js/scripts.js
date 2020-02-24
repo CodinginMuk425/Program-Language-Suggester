3ba7b3464158c4bedefd8243fd3f5ccb6387f35c
Ramy Carter <tbone425@msn.com>
2020-02-23 21:08:00 -0800

    remove assessment color and align

--- js/scripts.js
+++ js/scripts.js
@@ -2,53 +2,58 @@ $(document).ready(function () {
     //all responses will equal a cumulitive score and will create a score. The score will determine the language picker.
 
     //first question
-    var green = 1
-    var blue = 2
-    var red = 3
-    var yellow = 4
-    var orange = 5
+    var green = parseInt("1");
+    var blue = parseInt("2");
+    var red = parseInt("3");
+    var yellow = parseInt("4");
+    var orange = parseInt("5");
 
     $("button#green").click(function () {
         $("body").removeClass();
         $("body").addClass("green-background");
+        $("green").parseInt("1");
     });
 
     $("button#blue").click(function () {
         $("body").removeClass();
         $("body").addClass("blue-background");
+        $("blue").parseInt("2");
     });
 
     $("button#red").click(function () {
         $("body").removeClass();
         $("body").addClass("red-background");
+        $("red").parseInt("3");
     });
 
     $("button#yellow").click(function () {
         $("body").removeClass();
         $("body").addClass("yellow-background");
+        $("yellow").parseInt("4");
     });
 
     $("button#orange").click(function () {
         $("body").removeClass();
         $("body").addClass("orange-background");
+        $("orange").parseInt("5");
     });
 
     //Sports quesiton
-    var base = 5
-    var foot = 4
-    var socc = 3
-    var basket = 2
-    var other = 1
+    var base = parseInt("5");
+    var foot = parseInt("4");
+    var socc = parseInt("3");
+    var basket = parseInt("2");
+    var other = parseInt("1");
     var sport = $("input:radio[name=sport]:checked").val();
 
     $("#different").click(function () {
         prompt("What's your favorite sport?");
     });
 
-    //Puzzles quesiton will be 10 points for yes and 20 for no
+    //Puzzles quesiton 
 
-    var yes = 10
-    var no = 5
+    var yes = parseInt("10");
+    var no = parseInt("5");
 
     $("#click1").click(function () {
         $("#nay").toggle();
