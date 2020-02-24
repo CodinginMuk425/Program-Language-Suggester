9944a7c9513632dca74550be82af9332f3bc4679
Ramy Carter <tbone425@msn.com>
2020-02-23 17:34:15 -0800

    add alert to question 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //first question
-    $("button#green").click(function() {
+    $("button#green").click(function () {
         $("body").removeClass();
         $("body").addClass("green-background");
     });
 
-    $("button#blue").click(function() {
+    $("button#blue").click(function () {
         $("body").removeClass();
         $("body").addClass("blue-background");
     });
 
-    $("button#red").click(function() {
+    $("button#red").click(function () {
         $("body").removeClass();
         $("body").addClass("red-background");
     });
 
-    $("button#yellow").click(function() {
+    $("button#yellow").click(function () {
         $("body").removeClass();
         $("body").addClass("yellow-background");
     });
 
-    $("button#orange").click(function() {
+    $("button#orange").click(function () {
         $("body").removeClass();
         $("body").addClass("orange-background");
     });
@@ -32,7 +32,23 @@ $(document).ready(function() {
     }
     //Puzzles quesiton will be 10 points for yes and 20 for no
 
+    var yes = 10
+    var no = 5
+
+    $("#click1").click(function () {
+        $("#nay").hide();
+    });
+    $("#click2").click(function () {
+        $("#yay").hide();
+    });
     
+    $("#nay").click(function() {
+        alert("Yeah, puzzles suck!");
+    })
+
+    $("#yay").click(function() {
+        alert("Yeah, puzzles are great!");
+    })
 });
 
 
