fa118af670e14c09a84c8f288011a7e0a4aaadf6
Ramy Carter <tbone425@msn.com>
2020-03-07 14:00:15 -0800

    add fade out features

--- js/scripts.js
+++ js/scripts.js
@@ -14,30 +14,35 @@ $(document).ready(function () {
     $("button#green").click(function () {
         $("body").removeClass();
         $("body").addClass("green-background");
+        $("#colors").fadeOut();
         score = score + 1;
     });
 
     $("button#blue").click(function () {
         $("body").removeClass();
         $("body").addClass("blue-background");
+        $("#colors").fadeOut();
         score = score + 2;
     });
 
     $("button#red").click(function () {
         $("body").removeClass();
         $("body").addClass("red-background");
+        $("#colors").fadeOut();
         score = score + 3;
     });
 
     $("button#yellow").click(function () {
         $("body").removeClass();
         $("body").addClass("yellow-background");
+        $("#colors").fadeOut();
         score = score + 4;
     });
 
     $("button#orange").click(function () {
         $("body").removeClass();
         $("body").addClass("orange-background");
+        $("#colors").fadeOut();
         score = score + 5;
     });
 
@@ -45,23 +50,28 @@ $(document).ready(function () {
 
     var baseball = $("#baseball").click(function () {
         $(baseball = 10);
+        $("#sports").fadeOut();
     });
 
     var football = $("#football").click(function () {
         $(football = 9);
+        $("#sports").fadeOut();
     });
 
     var soccer = $("#soccer").click(function () {
         $(soccer = 8);
+        $("#sports").fadeOut();
     });
 
     var basketball = $("#basketball").click(function () {
         $(basketball = 7);
+        $("#sports").fadeOut();
     });
 
     var other = $("#other").click(function () {
         $(other = 6);
         prompt("What's your favoirte sport?")
+        $("#sports").fadeOut();
     });
 
     var sport = $("input:radio[name=sport]:checked").val();
@@ -71,11 +81,13 @@ $(document).ready(function () {
     var yes = $("#yes").click(function () {
         $(yes = 10);
         alert("Yeah puzzles are cool, I guess.")
+        $("#puzzles").fadeOut();
     });
 
     var no = $("#no").click(function () {
         $(no = 5);
         alert("Yeah! Puzzles suck!")
+        $("#puzzles").fadeOut();
     });
 
     //question 4 airplane & 5 age
@@ -85,10 +97,10 @@ $(document).ready(function () {
         var airplaneinput = parseInt($("#airplane").val());
         var age = parseInt($("#age").val());
         score = score + (yes | no) + (baseball | football | soccer | basketball | other) + airplaneinput + age;
-        if (score > 60) {
+        if (score > 85) {
             result = "You're ready for C#!";
             alert(result);
-        } else if (score <= 59 && score >= 35) {
+        } else if (score <= 84 && score >= 55) {
             result = "You should study Python!";
             alert(result);
         } else {
@@ -100,7 +112,7 @@ $(document).ready(function () {
     //hide survey once complete
 
     $("#finalscore").click(function () {
-        $(".survey").hide();
+        $(".survey").fadeOut();
     });
 
 });
